5acd44ffe3a25929990a848c54d51734d59806ac
gunju.ko <gunju.ko@navercorp.com>
2021-09-25 22:40:04 +0900

    [Learning-Kotlin] junit5 추가

---
+++ Learning-Kotlin/src/test/kotlin/gunju/learning/Hello.kt
@@ -0,0 +1,14 @@
+package gunju.learning
+
+import org.junit.jupiter.api.Test
+
+/**
+ * @author 고건주 (gunju.ko@navercorp.com)
+ */
+class Hello {
+
+    @Test
+    internal fun test() {
+        println("Hello World")
+    }
+}
